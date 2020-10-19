d6362b28395684fc457bcbccd6f27b99eec43d33
jesper <cph-jr211@cphbusiness.dk>
2020-10-19 13:09:29 +0200

    bpmn io

--- si/rpc/src/main/java/com/example/rpc/server/Test.java
+++ si/rpc/src/main/java/com/example/rpc/server/Test.java
@@ -5,9 +5,43 @@ public class Test {
 
     public static void main(String[] args) {
 
-        Teacher t = new Teacher("Mathias", 25);
+        int[][] matrix = new int[8][8];
 
-        System.out.println(t.name);
+        //counts rows, every second row i want to start with white
+        // Every other row with black :)
+
+        //every second one has to swap color
+        int brickCounter = 0;
+
+        for (int i = 0; i < matrix.length; i++) {
+
+            if(i % 2 == 0){
+                brickCounter = 0;
+            }else{
+                brickCounter = 1;
+            }
+
+            for (int j = 0; j < matrix.length; j++) {
+                if(brickCounter % 2 == 0){
+                    matrix[i][j] = 0;
+                    brickCounter ++;
+                }else{
+                    matrix[i][j] = 1;
+                    brickCounter++;
+                }
+            }
+
+        }
+
+
+
+
+        for (int i = 0; i < matrix.length; i++) { //this equals to the row in our matrix.
+            for (int j = 0; j < matrix[i].length; j++) { //this equals to the column in each row.
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println(); //change line on console as row comes to end in the matrix.
+        }
 
     }
 
