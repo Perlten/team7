b875760782fc6b79cf5181c6c8f2e2ac6401fad8
Nikolai Perlt <nikolaiperlt@gmail.com>
2020-11-11 11:33:03 +0100

    dbc

---
+++ dm/designByContract/src/main/java/LetsDoDesignByContract.java
@@ -0,0 +1,27 @@
+import static org.valid4j.Assertive.*;
+import static org.valid4j.Validation.*;
+import static org.hamcrest.Matchers.*;
+
+public class LetsDoDesignByContract {
+    private int x;
+
+
+    public int divide(int a, int b) {
+        // P
+        require(a > 0, "a should be large then 0");
+        require(b != 0, "b can not be 0");
+        validate(a != b, otherwiseThrowing(IsLessThenBrightException.class));
+
+        // C
+        this.x = a / b;
+
+        // Q
+        ensure(this.x == a / b);
+        return ensure(this.x, is(a / b));
+    }
+
+    public int getX() {
+        return x;
+    }
+}
+
---
+++ dm/designByContract/src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        LetsDoDesignByContract c = new LetsDoDesignByContract();
+        System.out.println(c.divide(2,1));
+
+    }
+}
